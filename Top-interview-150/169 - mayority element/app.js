c3ffdc5bb5ce4d97d9f9528335672d0e1109c984
Alejandro Leonian <alejandro.leonian@humansecurity.com>
2023-10-13 13:05:59 -0500

    working on 189

--- Top-interview-150/169 - mayority element/app.js
+++ Top-interview-150/169 - mayority element/app.js
@@ -20,3 +20,33 @@ var majorityElement = function (nums) {
 };
 
 console.log(majorityElement([2, 2, 1, 1, 1, 2, 2]));
+
+/*
+
+ChatGPT's version
+
+var majorityElement = function(nums) {
+    let candidate, count = 0;
+
+    for (const num of nums) {
+        if (count === 0) {
+            candidate = num;
+        }
+
+        count += num === candidate ? 1 : -1;
+    }
+
+    return candidate;
+};
+
+We maintain a candidate variable and a count variable to keep track of the majority element.
+
+We iterate through the nums array, and if count becomes zero, we update the candidate to the current number.
+
+We adjust count based on whether the current number matches the candidate. If it matches, we increment count; otherwise, we decrement it.
+
+The final candidate will be the majority element.
+
+This version simplifies the code and improves readability. It uses the Boyer-Moore Majority Vote algorithm to find the majority element in O(n) time and with O(1) space complexity.
+
+*/
