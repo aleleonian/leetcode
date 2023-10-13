16b749e24d4dc65abe627d14ba5e28f939712766
Alejandro Leonian <alejandro.leonian@humansecurity.com>
2023-10-13 18:37:37 -0500

    works hardcoded

--- Top-interview-150/189 - rotate array/app.js
+++ Top-interview-150/189 - rotate array/app.js
@@ -6,26 +6,52 @@
 var rotate = function (nums, k) {
     // nums.reverse();
 
-    console.log(nums);
-    reverseIt(nums, 0, nums.length - 1);
-    console.log(nums);
-    reverseIt(nums, 0, k - 1);
-    console.log(nums);
-    reverseIt(nums, k, nums.length - 1);
+    let firstPointer = 0;
+    let secondPointer = nums.length - 1;
+
     console.log(nums);
 
-    function reverseIt(array, startingPoint, endingPoint) {
-        for (let i = 0; ; i++) {
-            let temp = array[endingPoint];
-            array[endingPoint] = array[startingPoint];
-            array[startingPoint] = temp;
-            if (i >= (Math.floor((endingPoint - startingPoint) / 2))) break;
-            endingPoint--;
-            startingPoint++;
-        }
+
+    for (let i = 0; i < (nums.length - 1) / 2; i++) {
+        let temp = nums[firstPointer];
+        nums[firstPointer] = nums[secondPointer];
+        nums[secondPointer] = temp;
+        firstPointer++;
+        secondPointer--;
     }
+
+    console.log(nums);
+
+    firstPointer = 0;
+    secondPointer = 2;
+
+    for (let i = 0; i < (3) / 2; i++) {
+        let temp = nums[firstPointer];
+        nums[firstPointer] = nums[secondPointer];
+        nums[secondPointer] = temp;
+        firstPointer++;
+        secondPointer--;
+    }
+
+    console.log(nums);
+
+    firstPointer = 3;
+    secondPointer = nums.length - 1;
+
+    for (let i = 0; i < (7) / 2; i++) {
+        let temp = nums[firstPointer];
+        nums[firstPointer] = nums[secondPointer];
+        nums[secondPointer] = temp;
+        firstPointer++;
+        secondPointer--;
+    }
+
+    console.log(nums);
+
+
+
 };
 
-rotate([1, 2, 3, 4, 5, 6, 7], 7);
+rotate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3);
 // rotate([-1, -100, 3, 99], 2);
 //[3,99,-1,-100]
