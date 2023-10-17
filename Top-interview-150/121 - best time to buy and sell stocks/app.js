17b80603f5bdbb16b97595b78e4b086a7a3c7a3c
Alejandro Leonian <alejandro.leonian@humansecurity.com>
2023-10-17 17:08:50 -0500

    working on 121'

--- Top-interview-150/121 - best time to buy and sell stocks/app.js
+++ Top-interview-150/121 - best time to buy and sell stocks/app.js
@@ -4,13 +4,42 @@
  */
 var maxProfit = function (prices) {
 
-    const lowestDay = Math.min(...prices);
+    let deals = new Map();
 
-    if (prices.indexOf(lowestDay) == prices.length - 1) return 0;
+    const sortedByPrice = [...prices];
 
-    const highestDay = Math.max(...prices.slice(prices.indexOf(lowestDay) + 1));
+    sortedByPrice.sort((a, b) => a - b);
 
-    return highestDay - lowestDay;
+    let highPrice, lowPrice;
+
+    for (let i = 0; i < sortedByPrice.length; i++) {
+
+        lowPrice = sortedByPrice[i];
+
+        console.log("lowPrice->" + lowPrice);
+
+        const newPricesSlice = prices.slice(prices.indexOf(lowPrice) + 1);
+
+        console.log("newPricesSlice->" + newPricesSlice);
+
+        for (let x = 0; x < newPricesSlice.length; x++) {
+            const highestPriceCandidate = newPricesSlice[x];
+            if (highestPriceCandidate <= lowPrice || highestPriceCandidate < highPrice) continue;
+            else {
+                if (!highPrice || highestPriceCandidate > highPrice) {
+                    highPrice = highestPriceCandidate;
+                }
+            }
+        }
+        deals.set(lowPrice, highPrice);
+    }
+    console.log(deals);
+
+    // return highPrice - lowPrice || 0;
 };
 
-console.log(maxProfit([2, 4, 1]));
+console.log(maxProfit([3, 2, 6, 5, 0, 3]));
+
+
+//[3,2,6,5,0,3]
+//[0,2,3,3,6,5]
